63c79ab519e648fa58c79fa2cc9b2845e53cedb6
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2025-06-07 19:44:55 +0100

    Add privacy policy tests and CI

--- src/routes/PrivacyPolicy.test.tsx
+++ src/routes/PrivacyPolicy.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+
 import PrivacyPolicy from './PrivacyPolicy';
 
-test('renders privacy policy heading', () => {
+test('renders heading', () => {
   render(<PrivacyPolicy />);
-  const heading = screen.getByRole('heading', { name: /privacy policy/i });
+  const heading = screen.getByRole('heading', {
+    level: 1,
+    name: /privacy policy/i
+  });
   expect(heading).toBeInTheDocument();
 });
