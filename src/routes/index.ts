8d12f6abc8f432964eda1e12e78bc4e28d8effc5
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2025-06-14 20:40:10 +0100

    feat(routes): add barrel export

---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+export { default as Home } from './Home';
+export { default as About } from './About';
+export { default as Brands } from './Brands';
+export { default as News } from './News';
+export { default as Responsibility } from './Responsibility';
+export { default as Contact } from './Contact';
+export { default as PrivacyPolicy } from './PrivacyPolicy';
