37ccedc1a550ced9c0414cf3b4ae9ce76f43e6f1
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2025-06-16 17:24:58 +0100

    feat(routes): dynamic links

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,3 +6,4 @@ export { default as Responsibility } from './Responsibility';
 export { default as Contact } from './Contact';
 export { default as PrivacyPolicy } from './PrivacyPolicy';
 export { default as Links } from './Links';
+export { default as LinkCard } from './LinkCard';
